da2607a32c9aa56d30ebd1210cf56dc7493b26fd
CHRISTEEN PAUL <chrispaul1311@gmail.com>
2024-07-31 20:09:45 +0530

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,15 @@
+//var //variable//
+//const a =2; // we cant change onse assigned //
+
+// const fs = require("fs");
+//
+// fs.copyFileSync("file1.txt" , " file2.txt");//
+
+var supervillains= import("supervillains");
+
+
+var Mysupervillains function Mysupervillains (){
+   mySupervillainsName = supervillains.random()
+ } //mySupervillainsName = supervillains.random()
+
+console.log(Mysupervillains);
